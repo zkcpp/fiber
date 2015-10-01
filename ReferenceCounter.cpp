b15a2d95c322ca7b0851197ae85e7d29dc8f8386
Snappy Sys <snappysys@gmail.com>
2015-09-30 21:10:18 -0700

    Add ReferenceCounter base class

---
+++ ReferenceCounter.cpp
@@ -0,0 +1,22 @@
+#include "fiber/ReferenceCounter.h"
+
+namespace fiber {
+
+ReferenceCounter::ReferenceCounter() : rc_(1) {
+}
+
+ReferenceCounter::~ReferenceCounter() {
+}
+
+void ReferenceCounter::ref() {
+  ++rc_;
+}
+
+void ReferenceCounter::unref() {
+  auto val = rc_.fetch_sub(1);
+  if (val == 1) {
+    delete this;
+  }
+}
+
+} // fiber
---
+++ ReferenceCounterTest.cpp
@@ -0,0 +1,38 @@
+#include "fiber/ReferenceCounter.h"
+#include <iostream>
+#include <assert.h>
+
+using namespace std;
+using namespace fiber;
+
+class TestCounter : public ReferenceCounter {
+ public:
+  static TestCounter* make(int* val) {
+    return new TestCounter(val);
+  }
+
+  ~TestCounter() override {
+    *val_ = 0;
+  }
+
+ private:
+  explicit TestCounter(int* val) : val_(val) {}
+  int* val_;
+};
+
+void testDestruct() {
+  int val = 1;
+  auto x = TestCounter::make(&val);
+  x->ref();
+  assert(val > 0);
+  x->unref();
+  assert(val > 0);
+  x->unref();
+  assert(val == 0);
+}
+
+
+main() {
+  testDestruct();
+  cout << "OK" << endl;
+}
